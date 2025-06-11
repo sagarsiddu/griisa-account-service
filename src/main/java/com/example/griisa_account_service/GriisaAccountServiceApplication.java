cac08be90b85d9cde415d5391deec07213b319d8
msidhlinga <sagarsiddu995@gmail.com>
2025-06-11 16:19:17 +0530

    Initial commit

---
+++ src/main/java/com/example/griisa_account_service/GriisaAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.griisa_account_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriisaAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriisaAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/griisa_account_service/GriisaAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.griisa_account_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GriisaAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
