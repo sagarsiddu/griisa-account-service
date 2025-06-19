af065e15c7c652a617d53d83217c519b150d7aa5
msidhlinga <sagarsiddu995@gmail.com>
2025-06-19 18:36:59 +0530

    Branch_004 : enabled swagger ui without authentication

--- src/main/java/com/example/griisa_account_service/security/SecurityConfig.java
+++ src/main/java/com/example/griisa_account_service/security/SecurityConfig.java
@@ -25,7 +25,11 @@ public class SecurityConfig {
                         .requestMatchers("/accounts/health",
                                 "/auth/login",
                                 "/auth/register",
-                                "/api/verify").permitAll()
+                                "/api/verify",
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
